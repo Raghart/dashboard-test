99c453f25c96e1aeb9fd3b6b470251e81674457d
Rafael <rafaelrojas36@outlook.com>
2026-06-20 21:24:55 -0400

    r setuped first domain endpoit

--- backend/adapters/http/index.ts
+++ backend/adapters/http/index.ts
@@ -1,3 +1,12 @@
 import express from "express";
 
 const app = express()
+const port: number = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
+});
