fe7a1e521a0c3c1db6224f3de4def62d6b168e8c
Rafael <rafaelrojas36@outlook.com>
2026-06-22 16:58:49 -0400

    r added file to store csv URLs constants

---
+++ backend/domain/csvUrls.ts
@@ -0,0 +1,1 @@
+export const CUSTOMERURL = "https://raw.githubusercontent.com/spdrio/Brazilian-E-Commerce-Public-Dataset-by-Olist/refs/heads/master/files/olist_customers_dataset.csv";
--- backend/infrastructure/fetchData.ts
+++ backend/infrastructure/fetchData.ts
@@ -2,12 +2,12 @@ import axios from "axios";
 import csv from "csv-parser";
 import { Readable } from "stream";
 import { Customer } from "../domain/csvTypes";
+import { CUSTOMERURL } from "../domain/csvUrls";
 
 const fetchCustomers = async () => {
-    const url = "https://raw.githubusercontent.com/spdrio/Brazilian-E-Commerce-Public-Dataset-by-Olist/refs/heads/master/files/olist_customers_dataset.csv";
-    const res = await axios.get(url);
-
+    const res = await axios.get(CUSTOMERURL);
     const stream = Readable.from(res.data);
+    
     return new Promise((res, rej) => {
         const resultsArr: Customer[] = [];
 
@@ -15,7 +15,7 @@ const fetchCustomers = async () => {
         .on("end", () => {
             console.log(`Download finished with ${resultsArr.length}`);
             res(resultsArr);
-        });
+        }).on("error", (err) => rej(err));
     });
 };
 const customersArr = await fetchCustomers()
