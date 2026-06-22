7369427777ce26fb8da7892f2279311d8de64712
Rafael <rafaelrojas36@outlook.com>
2026-06-22 18:54:29 -0400

    r upgraded fetchRawCustomers func to parse the raw into the expected data type

--- backend/infrastructure/fetchData.ts
+++ backend/infrastructure/fetchData.ts
@@ -1,17 +1,23 @@
 import axios from "axios";
 import csv from "csv-parser";
 import { Readable } from "stream";
-import { Customer } from "../domain/csvTypes";
+import { RawCustomer } from "../domain/csvTypes";
 import { CUSTOMERURL, ITEMORDERSURL } from "../domain/csvUrls";
 
-const fetchRawCustomers = async () : Promise<Customer[]> => {
+const fetchRawCustomers = async () : Promise<RawCustomer[]> => {
     const res = await axios.get(CUSTOMERURL);
     const stream = Readable.from(res.data);
 
     return new Promise((res, rej) => {
-        const resultsArr: Customer[] = [];
+        const resultsArr: RawCustomer[] = [];
 
-        stream.pipe(csv()).on("data", (row: Customer) => resultsArr.push(row))
+        stream.pipe(csv()).on("data", (row: any) => resultsArr.push({
+            customer_id: row.customer_id,
+            customer_unique_id: row.customer_unique_id,
+            customer_zip_code_prefix: parseInt(row.customer_zip_code_prefix),
+            customer_city: row.customer_city,
+            customer_state: row.customer_state
+        }))
         .on("end", () => {
             console.log(`Download finished with ${resultsArr.length}`);
             res(resultsArr);
@@ -33,3 +39,6 @@ const fetchRawItemOrders = async () => {
         }).on("error", (err) => rej(err));
     });
 };
+
+const t = await fetchRawCustomers();
+console.log(t);
