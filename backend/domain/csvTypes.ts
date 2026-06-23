d449b77ebd5abe34cb7a70a7b1915f48b0fd5fe2
Rafael <rafaelrojas36@outlook.com>
2026-06-22 21:03:27 -0400

    r implemmented RawProduct interface

--- backend/domain/csvTypes.ts
+++ backend/domain/csvTypes.ts
@@ -44,3 +44,15 @@ export interface RawOrder {
     order_delivered_customer_date: Date,
     order_estimated_delivery_date: Date
 };
+
+export interface RawProduct {
+    product_id: string,
+    product_category_name: string,
+    product_name_lenght: number,
+    product_description_lenght: number,
+    product_photos_qty: number,
+    product_weight_g: number,
+    product_length_cm: number,
+    product_height_cm: number,
+    product_width_cm: number
+};
