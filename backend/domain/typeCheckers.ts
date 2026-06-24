ea6cff855627b712ab9b04df0d7f83bf7873ac57
Rafael <rafaelrojas36@outlook.com>
2026-06-24 16:45:50 -0400

    r implemmented isOrderReview to confirm if obj fulfill RawOrderReview intf

--- backend/domain/typeCheckers.ts
+++ backend/domain/typeCheckers.ts
@@ -21,6 +21,15 @@ export const isOrderPayment = (data: unknown) : boolean => {
     && "payment_type" in data && "payment_installments" in data && "payment_value" in data;
 };
 
+const isOrderReview = (data: unknown) : boolean => {
+    if (!data) return false;
+    
+    return typeof data === "object" && "review_id" in data
+    && "order_id" in data && "review_score" in data && "review_comment_title" in data 
+    && "review_comment_message" in data && "review_creation_date" in data 
+    && "review_answer_timestamp" in data;
+};
+
 export const isRawObject = (data: unknown) => {
     return data && typeof data === "object";
 }
