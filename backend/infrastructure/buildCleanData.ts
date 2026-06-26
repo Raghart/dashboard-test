a02a7892e875ce3b1992f04c643ced04d2f7cf43
Rafael <rafaelrojas36@outlook.com>
2026-06-26 16:52:42 -0400

    r added checkCleanDatabase func to verify if there are files in the created DB

--- backend/infrastructure/buildCleanData.ts
+++ backend/infrastructure/buildCleanData.ts
@@ -1,3 +1,8 @@
-const checkCleanDatabase = async () => {
-    
+import { prisma } from "../prisma/prismaClient";
+
+const checkCleanDatabase = async () : Promise<boolean> => {
+    const count = await prisma.cleanCategName.count();
+    return count === 0;
 };
+
+console.log(await checkCleanDatabase())
