74ab58c487cb04fd8319d82652f9947dee2a601e
Rafael <rafaelrojas36@outlook.com>
2026-06-26 20:24:36 -0400

    r added CleanCustomer table in the DB and the prisma schema

--- backend/domain/cleanTypes.ts
+++
@@ -1,4 +0,0 @@
-export interface CategoryName {
-    product_category_name: string,
-    product_category_name_english: string
-};
---
+++ backend/prisma/migrations/20260627002353_added_clean_customer_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "clean"."customers" (
+    "customer_id" TEXT NOT NULL,
+    "customer_unique_id" TEXT NOT NULL,
+    "customer_zip_code_prefix" INTEGER NOT NULL,
+    "customer_city" TEXT NOT NULL,
+    "customer_state" TEXT NOT NULL,
+
+    CONSTRAINT "customers_pkey" PRIMARY KEY ("customer_id")
+);
