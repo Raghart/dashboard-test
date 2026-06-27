d3d4b8024a20216e67b1d186f4cdb290608a6a48
Rafael <rafaelrojas36@outlook.com>
2026-06-27 18:39:46 -0400

    r starting to implement buildGoldCustomers to build Gold data from the Clean data

--- backend/infrastructure/buildGoldData.ts
+++ backend/infrastructure/buildGoldData.ts
@@ -1,8 +1,22 @@
+import { GoldDimCustomer } from "../prisma/client/client";
 import { prisma } from "../prisma/prismaClient";
 
 const checkGoldDatabase = async () : Promise<boolean> => {
     const dimCount = await prisma.goldDimCustomer.count();
     return dimCount === 0;
+};
+
+const buildGoldCustomers = async () => {
+    const cleanCustomers = await prisma.cleanCustomer.findMany();
+    let goldCustomers: GoldDimCustomer[] = [];
+
+    if (goldCustomers.length > 0) {
+        await prisma.goldDimCustomer.createMany({
+            data: goldCustomers,
+        });
+    };
+
+    console.log("The Gold Dimension for Customers has been sucessfully processed!")
 };
 
 const buildGoldLayer = async () => {
@@ -11,7 +25,13 @@ const buildGoldLayer = async () => {
         return
     }
 
-    const buildGoldFuncs = []; 
+    const buildGoldFuncs = [
+        buildGoldCustomers,
+    ];
+
+    for (const buildFunc of buildGoldFuncs) {
+        await buildFunc();
+    }
 };
 
 await buildGoldLayer();
