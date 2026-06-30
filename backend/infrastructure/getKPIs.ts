b7b0de65f8974bccf76c0bb44f5014b57acacf70
Rafael <rafaelrojas36@outlook.com>
2026-06-29 20:52:51 -0400

    r implemmented getKPI_AOV func to get the required KPI data

--- backend/infrastructure/getKPIs.ts
+++ backend/infrastructure/getKPIs.ts
@@ -1,5 +1,20 @@
 import { prisma } from "../prisma/prismaClient";
 
-const getKPIOrders = async () => {
+const getKPIOrders = async () : Promise<number> => {
     return await prisma.goldDimOrder.count();
 };
+
+const getKPI_AOV = async () : Promise<number> => {
+    const totalOrders = await prisma.goldDimOrder.count();
+    if (totalOrders === 0) return 0;
+
+    const result = await prisma.goldFactSales.aggregate({
+        _sum: {
+            payment_value_allocated: true,
+        }
+    });
+
+    return (result._sum.payment_value_allocated || 0) / totalOrders;
+};
+
+console.log(await getKPI_AOV());
